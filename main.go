1f4f165af0ed50cfa644fdfa9710421b023173ab
Mike Petersen <mike@odania-it.de>
2019-08-18 17:29:52 +0200

    Bumped version

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 const (
 	ExtensionName = "terraless-extension-aws-extend-switch-roles"
-	VERSION       = "0.1.0"
+	VERSION       = "0.1.1"
 )
 
 type ExtensionAwsExtendSwitchRoles struct {
